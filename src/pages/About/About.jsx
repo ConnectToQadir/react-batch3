0563f595f5862e25798d8d106a20c33c2914a2b4
Ghulam_Qadir <connecttoqadir@gmail.com>
2023-06-08 17:18:34 +0500

    useEffect useRef

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,51 @@
+import { useState, useEffect, useRef } from "react"
 
 const About = () => {
+
+
+  const [blogs, setBlogs] = useState([])
+
+
+  const [num, setNum] = useState(1)
+
+  const fetchBlogs = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const jsonRes = await res.json()
+    setBlogs(jsonRes)
+  }
+
+
+
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [num])
+
+
   return (
     <div>
       <h1>About Us</h1>
+
+      <h2>{num}</h2>
+      <button onClick={() => setNum(num + 1)} >Inc</button>
+
+
+      <div className="blogs">
+
+        {
+          blogs.map((v, i) => {
+            return(
+            <article key={i} >
+              <h1>{v.title}</h1>
+              <p>{v.body}</p>
+              <hr />
+            </article>
+            )
+          })
+        }
+
+      </div>
+
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
   var [num,setNum] = useState(1)
 
 
+  
+
+
 
 
 
